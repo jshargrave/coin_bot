d3914f1c9c3623118d71d620739a0f17d0d4e2b3
jshargrave <happyguyhere@hotmail.com>
2020-01-13 18:22:07 -0600

    Added logic to update variables when buying coins

--- bot_trade.py
+++ bot_trade.py
@@ -1,13 +1,16 @@
 import os
 import json
+import datetime
 
 
 class BotTrade:
-    def __init__(self, balance_file="data\\balance.json", trade_file="data\\trade.json"):
+    def __init__(self, balance_file="data\\balance.json", trade_file="data\\trade.json", simulated=True):
         self.balance_file = balance_file
         self.trade_file = trade_file
         self._balance = self.get_balance()
         self._trade_list = self.get_trade()
+        self.simulated = simulated
+        self.trade_count = len(self._trade_list)
 
 
         self.save_all()
@@ -50,8 +53,36 @@ class BotTrade:
 
     def save_balance(self):
         with open(self.balance_file, 'w') as outfile:
-            json.dump(self._balance, outfile)
+            json.dump(self._balance, outfile, indent=4, sort_keys=True)
 
     def save_trade(self):
         with open(self.trade_file, 'w') as outfile:
-            json.dump(self._trade_list, outfile)
+            trade_sorted = sorted(self._trade_list, key=lambda k: k['Time'], reverse=True)
+            json.dump(trade_sorted, outfile, indent=4, sort_keys=True)
+
+    def buy_coin(self, coin, coin_price, amount, max_cost=-1):
+        total_cost = coin_price * amount
+
+        # Make sure total cost is not more than max cost
+        if max_cost != -1 and max_cost < total_cost:
+            print("Could not buy coin ({}) because Total Cost ({}) is more than Max Cost ({}).".format(coin, total_cost, max_cost))
+            return
+
+        # Make sure enough USD in balance
+        if self._balance["USD"] < total_cost:
+            print("Could not buy coin ({}) because USD Balance ({}) is less than Total Cost ({})".format(coin, self._balance["USD"], total_cost))
+            return
+
+        trade_data = {
+            "Trade number": self.trade_count,
+            "Coin": coin,
+            "Buy Price": coin_price,
+            "Amount": amount,
+            "Total Cost": total_cost,
+            "Time": str(datetime.datetime.now())
+        }
+        self._trade_list.append(trade_data)
+        self._balance[coin] += amount
+        self._balance["USD"] -= total_cost
+        self.trade_count += 1
+        self.save_all()
