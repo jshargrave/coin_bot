83b2d2beb92a48a0c9cdaa6b76ef8ab245c2ed1e
jshargrave <happyguyhere@hotmail.com>
2020-01-16 08:13:16 -0600

    Added inheritance from coin base client module

--- coin_base_api.py
+++ coin_base_api.py
@@ -1,10 +1,18 @@
+from coinbase.wallet.client import Client
 import requests
+import json
 
 
-class CoinBaseAPI:
+class CoinBaseAPI(Client):
     def __init__(self, api_key, secret_key):
+        # Call super class constructor
+        super(CoinBaseAPI, self).__init__(api_key, secret_key)
+
+        # Save variables
         self.key = api_key
         self.secret = secret_key
+
+        self.accounts = self.get_accounts()
 
     @staticmethod
     def get_historical_price(coin, date):
@@ -16,3 +24,15 @@ class CoinBaseAPI:
     def get_price(coin):
         r = requests.get('https://api.coinbase.com/v2/prices/'+coin+'-USD/spot?currency=USD')
         return r.json()['data']['amount']
+
+    def parse_accounts(self):
+        return json.dumps(self.accounts.data)
+
+    def parse_accounts_balance(self):
+        account_balance = {}
+        for account in self.accounts.data:
+            account_balance[account.currency] = account.balance.amount
+        return account_balance
+
+    def refresh_all(self):
+        self.accounts.refresh()
